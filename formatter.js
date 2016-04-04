cce188915d58545991a749f9b8ec2e8e8a922fdc
Josh Brenden <joshuabrenden@gmail.com>
2016-04-04 07:35:18 -0600

    Changes to page spacing and whitespace behavior.

--- formatter.js
+++ formatter.js
@@ -1,13 +1,13 @@
 function format(){
   var rawData = document.getElementById("rawData").value;
-  var words = rawData.split(' ');
-
-  var delimitedWords = words.map(function(a) { return "'" + a + "'"; }).join(", ");
-
-   document.getElementById("formattedData").value = delimitedWords;
+  if(rawData !== ""){
+    var words = rawData.split(' ');
+    var delimitedWords = words.map(function(a) { return "'" + a + "'"; }).join(", ");
+    document.getElementById("formattedData").value = delimitedWords;
+  }
 }
 
-function clear(){
-  document.getElementById("rawData").value = "";
-  document.getElementById("formattedData").value = "";
+function clearText(){
+  document.getElementById("rawData").value = '';
+  document.getElementById("formattedData").value = '';
 }
